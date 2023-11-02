96befe87b4b66d3f964002854554eac2536f985b
Nikita <nek.55rumm@gmail.com>
2023-11-02 19:53:26 +0300

    9 end

--- ----------------------------1/scenes/game_object/basic_enemy/basic_enemy.gd
+++ ----------------------------1/scenes/game_object/basic_enemy/basic_enemy.gd
@@ -3,9 +3,7 @@ extends CharacterBody2D
 const MAX_SPEED = 40
 
 @onready var health_component: HealthComponent = $HealthComponent
-
-
-
+@onready var visuals = $Visuals
 
 
 
@@ -14,6 +12,10 @@ func _process(delta: float) -> void:
 	velocity = direction * MAX_SPEED
 	
 	move_and_slide()
+	
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(-move_sign, 1)
 
 func get_direction_to_player():
 	var player_node = get_tree().get_first_node_in_group("player") as Node2D

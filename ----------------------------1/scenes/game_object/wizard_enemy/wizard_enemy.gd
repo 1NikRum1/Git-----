11f30e6c03fc4548336fa4da4385220c3c577477
Nikita <nek.55rumm@gmail.com>
2023-11-07 15:01:33 +0300

    10 end

--- ----------------------------1/scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ ----------------------------1/scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -1,19 +1,16 @@
 extends CharacterBody2D
 
+@onready var velocity_component: Node = $VelocityComponent
+@onready var visuals: Node2D = $Visuals
 
-@onready var visuals = $Visuals
-@onready var velocity_component = $VelocityComponent
+
 
 
 func _process(delta: float) -> void:
 	velocity_component.accelerate_to_player()
 	velocity_component.move(self)
 	
-	
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
-		visuals.scale = Vector2(-move_sign, 1)
-
-
-
+		visuals.scale = Vector2(move_sign, 1)
 
